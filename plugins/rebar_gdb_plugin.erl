1a69a23ba4e489212fbdb83ea806a0111409100c
Jean Rouge <jer329@cornell.edu>
2017-01-18 17:25:43 -0800

    blah

--- plugins/rebar_gdb_plugin.erl
+++ plugins/rebar_gdb_plugin.erl
@@ -1,6 +1,18 @@
 -module(rebar_gdb_plugin).
 -compile(export_all).
 
+%% can also come in handy: https://sideshowcoder.com/post/tag/gdb and
+%% https://groups.google.com/forum/#!topic/erlang-programming/Gz2zbQjC144
+%% TL;DR: vi $(which erl) and
+%%
+%% if [ ! -z "$USE_GDB" ]; then
+%% gdb $BINDIR/erlexec --args $BINDIR/erlexec ${1+"$@"}
+%% else
+%% exec $BINDIR/erlexec ${1+"$@"}
+%% fi
+%%
+%% or else gdb --eval-command=run --eval-command=quit $BINDIR/erlexec --args $BINDIR/erlexec ${1+"$@"}
+
 pre_eunit(_Config, _AppFile) ->
     case os:getenv("USE_GDB") of
         false ->
